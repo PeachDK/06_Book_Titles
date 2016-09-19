2674ae26bd1f8c85a5835c5a4fca1e28046511f5
Pelle <Pelle@LAPTOP-T69PBCOE>
2016-09-19 09:56:38 +0200

    Add project files.

---
+++ 06_Book_Titles/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace _06_Book_Titles
+{
+    internal class Book
+    {
+        private string v = "";
+        public string Title
+        {
+
+
+            get
+            {
+                return v;
+            }
+
+            set
+            {
+
+                v = value;
+                CultureInfo cultureInfo = new CultureInfo("en-US");
+                TextInfo textInfo = cultureInfo.TextInfo;
+                //Array contains word that should not be capitalized.
+                string[] fy = { "and", "over", "the", "an", "a", "in", "of" };
+
+                //inputs string v into a array. Using build in method spilt.
+                string[] s = v.Split();
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[0] != null)
+                    {
+                        s[0] = textInfo.ToTitleCase(s[0]);
+                    }
+
+                    bool a = Array.Exists(fy, element => element == s[i]);
+                    if (a == false)
+                    {
+                        s[i] = textInfo.ToTitleCase(s[i]);
+                    }
+
+
+                }
+                v = string.Join(" ", s);
+
+            }
+        }
+    }
+}
